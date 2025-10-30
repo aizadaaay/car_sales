4384584ea244fc53f600aaf208e06b72763f8353
aizadaaay <159110522+aizadaaay@users.noreply.github.com>
2025-10-31 03:02:32 +0500

    Comment out MEDIA_ROOT in settings.py
    Comment out MEDIA_ROOT setting for future reference.

--- car_sales/settings.py
+++ car_sales/settings.py
@@ -163,5 +163,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = '/media/'
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
